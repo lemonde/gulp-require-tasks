f23653c2030df0e72219da52544263e8bf09e1e0
Jonathan Jalouzot <jalouzot@lemonde.fr>
2016-03-29 17:27:31 +0200

    fix: blacklist node_modules if use npm link

--- index.js
+++ index.js
@@ -23,8 +23,10 @@ function gulpRequireTasks (options) {
 
   // Recursively visiting all modules in the specified directory
   // and registering Gulp tasks.
+  blacklist = /node_modules/,
   requireDirectory(module, options.path, {
-    visit: moduleVisitor
+        visit: moduleVisitor,
+        exclude: blacklist
   });
 
   /**
